173243d6654e88c59dbf7d84382162a27950e63f
Abdulla38 <abdullakurmanbaev38@gmail.com>
2022-11-23 12:17:46 +0600

    Пересоздал структуру

---
+++ bot/handlers/user.py
@@ -0,0 +1,24 @@
+from aiogram import Dispatcher, types
+from bot.keyboards import markup
+
+
+async def start(message: types.Message):
+    text = f'Привет.'
+    text2 = f'Я бот журнал.'
+    text3 = f'Я записиваю в мою базу данных учеников которые не пришли в школу.\n'
+    text3 += f'Узнать по подробнее "Подробнее"'
+    await message.bot.send_message(message.from_user.id, text, reply_markup=markup)
+    await message.bot.send_message(message.from_user.id, text2)
+    await message.bot.send_message(message.from_user.id, text3)
+    if message.text == 'Подробнее':
+        await more()
+
+
+async def more(message: types.Message):
+    resp_msg = "/write - записать список учеников\n/write_down - записать отсуствещих\n/show_now - показать отсуствущих на сегодня\n"
+    await message.bot.send_message(message.from_user.id, resp_msg)
+
+
+def register_user_handler(dp: Dispatcher):
+    dp.register_message_handler(start, commands='start')
+    dp.register_message_handler(more, content_types='texr')
--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import asyncio
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import Bot, Dispatcher, executor
 import logging
 from bot.env import Env
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-
-from bot.keyboards import markup
+from bot.handlers.user import register_user_handler
 
 # Create bot, loop and logging
 logging.basicConfig(level=logging.INFO)
@@ -14,23 +13,8 @@ loop = asyncio.get_event_loop()
 
 
 # Create start handler
-@dp.message_handler(commands='start')
-async def start(message: types.Message):
-    text = f'Привет.'
-    text2 = f'Я бот журнал.'
-    text3 = f'Я записиваю в мою базу данных учеников которые не пришли в школу.\n'
-    text3 += f'Узнать по подробнее "Подробнее"'
-    await bot.send_message(message.from_user.id, text, reply_markup=markup)
-    await bot.send_message(message.from_user.id, text2)
-    await bot.send_message(message.from_user.id, text3)
-    if message.text == 'Подробнее':
-        await more()
+register_user_handler(dp)
 
-
-@dp.message_handler(content_types='text')
-async def more(message: types.Message):
-    resp_msg = "/write - записать список учеников\n/write_down - записать отсуствещих\n/show_now - показать отсуствущих на сегодня\n"
-    await bot.send_message(message.from_user.id, resp_msg)
 
 # run bot
 if __name__ == '__main__':
